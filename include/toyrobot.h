7c22ae6b532eb94f97fcacdf52b689ec80e9173a
Simon Puligny <simon.puligny@uptime.ac>
2022-08-29 17:08:09 +0200

    implement ToyRobot class.

--- include/toyrobot.h
+++ include/toyrobot.h
@@ -1,11 +1,40 @@
 #ifndef TOYROBOT_H
 #define TOYROBOT_H
 
+#include <string>
+
+enum direction {
+    NONE = -1,
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST
+};
 
 class ToyRobot
 {
 public:
-    ToyRobot();
+    ToyRobot(unsigned int width=5, unsigned int  height=5);
+
+    bool move();
+    bool place(unsigned int x, unsigned int y, direction dir);
+    void rotate(bool right);
+    void report();
+
+    int x() const;
+    int y() const;
+    enum direction direction() const;
+    std::string getDirection() const;
+    bool isPlaced() const;
+
+private:
+    unsigned int mWidth;
+    unsigned int mHeight;
+    bool mPlaced = false;
+
+    int mX;
+    int mY;
+    enum direction mDirection;
 };
 
 #endif // TOYROBOT_H
--- src/toyrobot.cpp
+++ src/toyrobot.cpp
@@ -1,6 +1,129 @@
 #include "toyrobot.h"
 
-ToyRobot::ToyRobot()
-{
+#include <iostream>
+#include <sstream>
 
+using namespace std;
+
+std::ostream& operator<<(std::ostream& os, direction c)
+{
+    switch(c)
+    {
+        case NONE  : os << "NONE";  break;
+        case NORTH   : os << "NORTH";   break;
+        case EAST : os << "EAST"; break;
+        case SOUTH   : os << "SOUTH";   break;
+        case WEST  : os << "WEST";  break;
+        default    : os.setstate(std::ios_base::failbit);
+    }
+    return os;
+}
+
+ToyRobot::ToyRobot(unsigned int width, unsigned int height)
+    : mWidth(width)
+    , mHeight(height)
+    , mX(-1)
+    , mY(-1)
+    , mDirection(NONE)
+{
+}
+
+bool ToyRobot::move()
+{
+    if (!mPlaced)
+        return false;
+
+    switch (mDirection) {
+
+    case NONE:
+        return false;
+    case NORTH:
+        if (mY >= 4)
+            return false;
+        else
+            mY++;
+        break;
+    case EAST:
+        if (mX >= 4)
+            return false;
+        else
+            mX++;
+        break;
+    case SOUTH:
+        if (mY <= 0)
+            return false;
+        else
+            mY--;
+        break;
+    case WEST:
+        if (mX <= 0)
+            return false;
+        else
+            mX--;
+        break;
+    default:
+        return false;
+    }
+
+    return true;
+}
+
+bool ToyRobot::place(unsigned int x, unsigned int y, enum direction dir)
+{
+    if (x >= mWidth || y >= mHeight) {
+        cerr << "cannot place robot, coordinates are out of range." << endl;
+        return false;
+    }
+
+    mX = x;
+    mY = y;
+    mDirection = dir;
+    mPlaced = true;
+    return true;
+}
+
+void ToyRobot::rotate(bool right)
+{
+    if (!mPlaced)
+        return;
+
+    if (right)
+        mDirection = (enum direction)(((int)mDirection + 1) % 4);
+    else
+        mDirection = (enum direction)(((int)mDirection - 1) % 4);
+}
+
+void ToyRobot::report()
+{
+    if (!mPlaced)
+        return;
+
+    cout << mX << ',' << mY << ',' << mDirection << endl;
+}
+
+int ToyRobot::x() const
+{
+    return mX;
+}
+
+int ToyRobot::y() const
+{
+    return mY;
+}
+
+direction ToyRobot::direction() const
+{
+    return mDirection;
+}
+
+string ToyRobot::getDirection() const
+{
+    stringstream s;
+    s << mDirection;
+    return s.str();
+}
+
+bool ToyRobot::isPlaced() const
+{
+    return mPlaced;
 }
